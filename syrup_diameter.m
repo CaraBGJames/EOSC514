5269e1a9418d24cb44ee0fd6cfd05307e4592ffa
CaraBGJames <carajames@outlook.com>
2022-04-01 11:52:20 -0700

    More work done on syrup diameter

--- syrup_diameter.m
+++ syrup_diameter.m
@@ -4,8 +4,9 @@ l = 19;
 framename = [file_loc,'nooil_frame_', num2str(l,'%04.f'),'.jpg'];
 b = imread(framename);
 b1 = b(210:850,610:1250,3);
+%%
 
-for k = 20:1798
+for k = 90
     framename = [file_loc,'nooil_frame_', num2str(k,'%04.f'),'.jpg'];
     f = imread(framename);
     f1 = f(210:850,610:1250,3);
@@ -18,6 +19,20 @@ for k = 20:1798
     edges(k-18,:) = a1;
 end
 
+%% different method
+se = strel('disk',20);
+
+for k = 300
+    framename = [file_loc,'nooil_frame_', num2str(k,'%04.f'),'.jpg'];
+    f = imread(framename);
+    f1 = f(210:850,610:1250,3);
+    f2 = b1-f1;
+    f3 = im2double(f2);
+    f4 = f3 > 0.2;
+    f5 = bwareafilt(f4,1);
+    f6 = imclose(f5, se)
+    imshow(imfuse(f1,f6));
+end
 
 %%
 time = 29:1798;
